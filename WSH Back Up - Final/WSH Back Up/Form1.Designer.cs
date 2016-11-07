a3bcba56a55aa95de6b0e4faac65483e1a21c759
Simeon Lee <ipzs3mini@gmail.com>
2016-11-07 02:31:44 +0000

    Adding Project Files
    Adding Project Files from existing project.

---
+++ WSH Back Up - Final/WSH Back Up/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WSH_Back_Up
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.textBoxNetwork = new System.Windows.Forms.TextBox();
+            this.textBoxCloud = new System.Windows.Forms.TextBox();
+            this.labelSource = new System.Windows.Forms.Label();
+            this.lableNetwork = new System.Windows.Forms.Label();
+            this.labelCloud = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonSrcBrowse = new System.Windows.Forms.Button();
+            this.buttonNetworkBrowse = new System.Windows.Forms.Button();
+            this.buttonCloudBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(217, 305);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 25);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(67, 305);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(100, 25);
+            this.btnBackup.TabIndex = 1;
+            this.btnBackup.Text = "Back Up";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(58, 82);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(190, 20);
+            this.textBoxSource.TabIndex = 2;
+            this.textBoxSource.Text = "C:\\Users\\Admin\\Desktop\\Source";
+            // 
+            // textBoxNetwork
+            // 
+            this.textBoxNetwork.Location = new System.Drawing.Point(58, 139);
+            this.textBoxNetwork.Name = "textBoxNetwork";
+            this.textBoxNetwork.Size = new System.Drawing.Size(190, 20);
+            this.textBoxNetwork.TabIndex = 3;
+            this.textBoxNetwork.Text = "C:\\Users\\Admin\\Desktop\\Network";
+            // 
+            // textBoxCloud
+            // 
+            this.textBoxCloud.Location = new System.Drawing.Point(58, 198);
+            this.textBoxCloud.Name = "textBoxCloud";
+            this.textBoxCloud.Size = new System.Drawing.Size(190, 20);
+            this.textBoxCloud.TabIndex = 4;
+            this.textBoxCloud.Text = "C:\\Users\\Admin\\Desktop\\Cloud";
+            // 
+            // labelSource
+            // 
+            this.labelSource.AutoSize = true;
+            this.labelSource.Location = new System.Drawing.Point(55, 66);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(41, 13);
+            this.labelSource.TabIndex = 5;
+            this.labelSource.Text = "Source";
+            // 
+            // lableNetwork
+            // 
+            this.lableNetwork.AutoSize = true;
+            this.lableNetwork.Location = new System.Drawing.Point(55, 123);
+            this.lableNetwork.Name = "lableNetwork";
+            this.lableNetwork.Size = new System.Drawing.Size(103, 13);
+            this.lableNetwork.TabIndex = 6;
+            this.lableNetwork.Text = "Network Destination";
+            // 
+            // labelCloud
+            // 
+            this.labelCloud.AutoSize = true;
+            this.labelCloud.Location = new System.Drawing.Point(55, 182);
+            this.labelCloud.Name = "labelCloud";
+            this.labelCloud.Size = new System.Drawing.Size(90, 13);
+            this.labelCloud.TabIndex = 7;
+            this.labelCloud.Text = "Cloud Destination";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(132, 30);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(120, 24);
+            this.labelTitle.TabIndex = 8;
+            this.labelTitle.Text = "Back Up App";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonSrcBrowse
+            // 
+            this.buttonSrcBrowse.Location = new System.Drawing.Point(255, 80);
+            this.buttonSrcBrowse.Name = "buttonSrcBrowse";
+            this.buttonSrcBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonSrcBrowse.TabIndex = 9;
+            this.buttonSrcBrowse.Text = "Browse";
+            this.buttonSrcBrowse.UseVisualStyleBackColor = true;
+            this.buttonSrcBrowse.Click += new System.EventHandler(this.buttonSrcBrowse_Click);
+            // 
+            // buttonNetworkBrowse
+            // 
+            this.buttonNetworkBrowse.Location = new System.Drawing.Point(255, 137);
+            this.buttonNetworkBrowse.Name = "buttonNetworkBrowse";
+            this.buttonNetworkBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonNetworkBrowse.TabIndex = 10;
+            this.buttonNetworkBrowse.Text = "Browse";
+            this.buttonNetworkBrowse.UseVisualStyleBackColor = true;
+            this.buttonNetworkBrowse.Click += new System.EventHandler(this.buttonNetworkBrowse_Click_1);
+            // 
+            // buttonCloudBrowse
+            // 
+            this.buttonCloudBrowse.Location = new System.Drawing.Point(255, 196);
+            this.buttonCloudBrowse.Name = "buttonCloudBrowse";
+            this.buttonCloudBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonCloudBrowse.TabIndex = 11;
+            this.buttonCloudBrowse.Text = "Browse";
+            this.buttonCloudBrowse.UseVisualStyleBackColor = true;
+            this.buttonCloudBrowse.Click += new System.EventHandler(this.buttonCloudBrowse_Click_1);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(55, 241);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(48, 13);
+            this.labelProgress.TabIndex = 12;
+            this.labelProgress.Text = "Progress";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(58, 257);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(272, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.buttonCloudBrowse);
+            this.Controls.Add(this.buttonNetworkBrowse);
+            this.Controls.Add(this.buttonSrcBrowse);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelCloud);
+            this.Controls.Add(this.lableNetwork);
+            this.Controls.Add(this.labelSource);
+            this.Controls.Add(this.textBoxCloud);
+            this.Controls.Add(this.textBoxNetwork);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Back Up App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.TextBox textBoxNetwork;
+        private System.Windows.Forms.TextBox textBoxCloud;
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.Label lableNetwork;
+        private System.Windows.Forms.Label labelCloud;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonSrcBrowse;
+        private System.Windows.Forms.Button buttonNetworkBrowse;
+        private System.Windows.Forms.Button buttonCloudBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog3;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WSH Back Up - Final/WSH Back Up/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace WSH_Back_Up
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        //  Exit button
+        //
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //
+        // Source Browse Button
+        //    
+        private void buttonSrcBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxSource.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        //
+        // Network Browse Button
+        //            
+        private void buttonNetworkBrowse_Click_1(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxNetwork.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        //
+        // Cloud Browse Button       
+        //
+        private void buttonCloudBrowse_Click_1(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog3.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxCloud.Text = folderBrowserDialog3.SelectedPath;
+            }
+        }
+
+        //
+        //  Back Up button
+        //
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            // Sets strings as per textbox.text
+            string source = textBoxSource.Text;
+            string network = textBoxNetwork.Text;
+            string cloud = textBoxCloud.Text;
+            
+            // Creates Destination directories as per Source unless source not chosen
+            // Network
+                   if (Directory.Exists(source))
+                   foreach (string dirPath in Directory.GetDirectories(source, "*",
+                       SearchOption.AllDirectories))
+                       Directory.CreateDirectory(network + dirPath.Remove(0, source.Length));
+            // Cloud
+                   if (Directory.Exists(source))
+                   foreach (string dirPath in Directory.GetDirectories(source, "*",
+                       SearchOption.AllDirectories))
+                       Directory.CreateDirectory(cloud + dirPath.Remove(0, source.Length));
+
+            // Copies files as per Source unless destination not chosen
+            // Network
+                   if (Directory.Exists(network))
+                   foreach (string newPath in Directory.GetFiles(source, "*.*",
+                       SearchOption.AllDirectories))
+                       File.Copy(newPath, network + newPath.Remove(0, source.Length), true);
+            // Cloud
+                   if (Directory.Exists(cloud))
+                   foreach (string newPath in Directory.GetFiles(source, "*.*",
+                       SearchOption.AllDirectories))
+                       File.Copy(newPath, cloud + newPath.Remove(0, source.Length), true);
+
+             
+
+
+        }
+            
+    }
+}
